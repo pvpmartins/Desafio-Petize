302959ef9cf4d2604e7b569217d5e108ea188f8b
Paulo <pvpmartins@hotmail.com>
2023-02-10 11:07:19 -0300

    deleted auth token

--- src/client/api.ts
+++ src/client/api.ts
@@ -4,5 +4,4 @@ import process from "process";
 
 export const api = axios.create({
   baseURL: "https://api.github.com/users/",
-  headers: { authorization: "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" },
 });
